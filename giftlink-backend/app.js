a2db244bf17852a2d42355fe4d26134db97f492c
Snamude <stella.namude@gmail.com>
2025-05-08 19:16:32 -0400

    message

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -36,7 +36,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use('/api/gifts', searchRoutes);
+app.use('/api/search', searchRoutes);
  
 // Global Error Handler
 app.use((err, req, res, next) => {
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/",(req,res)=>{
-    res.send("Inside the server")
+    res.send("Inside the server");
 })
 
 app.listen(port, () => {
