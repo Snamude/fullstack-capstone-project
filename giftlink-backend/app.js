877049f5e50905a0699ceee07618ff99e4a29c1d
Snamude <stella.namude@gmail.com>
2025-05-08 18:57:29 -0400

    Updated files again

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -6,8 +6,7 @@ const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
-
-
+ 
 const app = express();
 app.use("*",cors());
 const port = 3060;
@@ -17,18 +16,16 @@ connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
     .catch((e) => console.error('Failed to connect to DB', e));
-
-
+ 
 app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
-
-
+const searchRoutes = require('./routes/searchRoutes');
+ 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
@@ -36,12 +33,11 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
-
-
+app.use('/api/gifts', searchRoutes);
+ 
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
